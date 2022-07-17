e966c129cc9d21b8d1cacba1c8e074bf65ea9e9f
kkaiyawo <kraiwish123@gmail.com>
2022-07-17 11:28:26 +0700

    create rush(x, y)

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ void rush(int x, int y);
 
 int main(void)
 {
+    rush(5, 3);
     return (0);
 }
--- rush00.c
+++ rush00.c
@@ -1,6 +1,28 @@
+void ft_putchar(char c);
+
 void rush(int x, int y);
 
 void rush(int x, int y)
 {
-    //hello
+    for(int i = 0; i < y; i++)
+    {
+        for(int j = 0; j < x; j++)
+        {
+            if(i == 0 && j == 0 || i == 0 && j == x - 1 || i == y - 1 && j == 0 || i == y - 1 && j == x - 1)
+                ft_putchar('o');
+            else
+            {
+                if(i == 0 || i == y - 1)
+                    ft_putchar('-');
+                else
+                {
+                    if(j == 0 || j == x -1)
+                        ft_putchar('|');
+                    else
+                        ft_putchar(' ');
+                }
+            }
+        }
+        ft_putchar('\n');
+    }
 }
